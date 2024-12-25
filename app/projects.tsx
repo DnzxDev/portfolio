b93e20107aa0d74745b4d140899696d1b602a487
Daniel Angelo <102752827+Dn099z1@users.noreply.github.com>
2024-12-25 12:28:45 -0300

    Update projects.tsx

--- app/projects.tsx
+++ app/projects.tsx
@@ -21,21 +21,21 @@ const projects = [
     title: "Landing Page + Dashboard",
     description: "Now Yes! this project is a version of the first two projects, but much improved! here we have a landing page with the fixed dark theme, when you log in with discord, and access the dashboard, you can change themes, there is a shopping system, inventory and information, to buy something, you must have gems! In addition, when you buy something, a message is sent to an API in FiveM, the API does the work of adding the benefits, which are: Properties, Cars, Vips and more!",
     technologies: ["Next.js", "React", "Tailwind", "Nhost", "Typescript", "Javascript", "GraphQL"],
-    imageUrl: "https://media.discordapp.net/attachments/1260466849804456036/1320602340100935680/image.png?ex=676a3245&is=6768e0c5&hm=583ddf308535277f1e7f7c45dfb99e74925902501006f0aa16d0a861ec6f5a4c&=&format=webp&quality=lossless&width=1029&height=558",
+    imageUrl: "https://r2.fivemanage.com/h1RaORAfpynzso56Pcf6Z/image.png",
     isPrivate: true
   },
   {
     title: "Admin Panel",
     description: "Do you remember the previous project? the landing page together with a dashboard? Well, if there are to be products, there must be a way to add them. In the admin dashboard you can do this and others, such as supervising your server, players and bans!",
     technologies: ["Next.js", "React", "Tailwind", "Typescript", "Javascript", "MySQL"],
-    imageUrl: "https://media.discordapp.net/attachments/1286715591993856011/1320075291879538800/image.png?ex=676998eb&is=6768476b&hm=69f9c95aa4a5504d00262c583ca13c9a66b346680099d40faf3096cdcb4885b8&=&format=webp&quality=lossless&width=1035&height=557",
+    imageUrl: "https://r2.fivemanage.com/h1RaORAfpynzso56Pcf6Z/im23age.png",
     isPrivate: true
   },
   {
     title: "Storage",
     description: "This project acts as a storage for images and files, made for basic use, considering that only files inside the public folder are accepted, in the future it is possible to add support for some host platform to make file management easier!",
     technologies: ["Next.js", "React", "Tailwind", "Typescript"],
-    imageUrl: "https://media.discordapp.net/attachments/1260466849804456036/1320603413133660190/image.png?ex=676a3345&is=6768e1c5&hm=682526dc395a1c9a9c1bde8510d4f01c2e40a998777d22189a054298c7ca8d91&=&format=webp&quality=lossless&width=1025&height=558",
+    imageUrl: "https://r2.fivemanage.com/h1RaORAfpynzso56Pcf6Z/im1age.png",
     isPrivate: true
   },
   {
